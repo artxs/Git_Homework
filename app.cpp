e177c281fbd6158d2214249409206bc78b3d4549
Artūrs Greidāns <artiks2525@inbox.lv>
2015-12-11 20:29:23 +0200

    added multiplication of x and y in app.cpp

--- app.cpp
+++ app.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main()
 {
 	
-int x, y;
+int x, y, z ;
 
 cout<<"\n x value : ";
 cin>>x;
 cout<<"\n y value : ";
 cin>>y;
+
+z = x * y;
+
+cout<<"\n Multiplication of x and y : "<<z;
 
 	
 system("pause>>nul");
