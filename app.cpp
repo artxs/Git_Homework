4a91092ac216d0fe0134a29b8194593b722a5227
Artūrs Greidāns <artiks2525@inbox.lv>
2015-12-11 20:00:54 +0200

    added x and y variables app.cpp

--- app.cpp
+++ app.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
-#include <cstdlib>
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
 using namespace std;
-
 int main()
 {
+	
+int x, y;
 
+cout<<"\n x value : ";
+cin>>x;
+cout<<"\n y value : ";
+cin>>y;
 
 	
-system("pause>null");
+system("pause>>nul");
 return 0;
 }
